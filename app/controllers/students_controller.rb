2d0fe2d0674ee8b5b366f824a3b4ec79156b0ed8
Aaron Fowlkes <anbfowlkes@gmail.com>
2022-09-02 14:13:13 -0400

    commit tree 41e48fb0bbaf3a2a2ecfab98c6da4cc8fdc115e2

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,18 @@
+class StudentsController < ApplicationController
+
+    def index
+        render json: Student.all
+    end
+
+    def grades
+        render json: Student.order(grade: :desc)
+        # render json: Student.all.order(:grade)
+    end
+
+
+
+    def highest_grade
+        render json: Student.order(grade: :desc).first
+    end
+
+end
